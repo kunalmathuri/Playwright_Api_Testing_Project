b72688a138f3933c1823a1c31c7db797baa8b467
kunalmathuri <kunalkrmathuri@gmail.com>
2025-08-11 15:47:09 +0530

    All remainig CRUD & Allure reporting done!

--- playwright.config.js
+++ playwright.config.js
@@ -13,6 +13,8 @@ import { defineConfig, devices } from "@playwright/test";
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
+  //test timeout
+  timeout: 5 * 60 * 1000,
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -20,17 +22,23 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Opt out of parallel tests on CI & (for 5 workers = 5 browsers can run locally). */
+  workers: process.env.CI ? 1 : 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["html"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "https://restful-booker.herokuapp.com",
+    // baseURL: "https://restful-booker.herokuapp.com",
+    //video, screenshot, headless mode
+    video: "on",
+    screenshot: "on",
+    headless: false,
+    //custom attribute
+    testIdAttribute: "autocomplete",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "on",
   },
 
   /* Configure projects for major browsers */
---
+++ tests/patch_api_request_08.spec.js
@@ -0,0 +1,85 @@
+//Load Playwright module
+import { test, expect } from "@playwright/test";
+import { stringFormat } from "../utils/comman";
+
+const bookingAPIrequestBody = require("../test-data/post_dynamic_request_body.json");
+const tokenRequestBody = require("../test-data/token_request_body.json");
+const patchRequestBody = require("../test-data/patch_request_body.json");
+
+//write a test
+console.log("==POST API==");
+
+//Create POST api request
+test("Create PUT api request in Playwright", async ({ request }) => {
+  const dynamicRequestBody = stringFormat(
+    JSON.stringify(bookingAPIrequestBody),
+    "Kunal",
+    "Mathuri",
+    "Mango"
+  );
+
+  const postAPIResponse = await request.post("/booking", {
+    data: JSON.parse(dynamicRequestBody),
+  });
+  //Validate Status Code
+
+  expect(postAPIResponse.ok()).toBeTruthy();
+  expect(postAPIResponse.status()).toBe(200);
+
+  const postAPIResponseBody = await postAPIResponse.json();
+  console.log(postAPIResponseBody);
+
+  const bId = postAPIResponseBody.bookingid;
+
+  //validate JSON api response
+  expect(postAPIResponseBody.booking).toHaveProperty("firstname", "Kunal");
+  expect(postAPIResponseBody.booking).toHaveProperty("lastname", "Mathuri");
+
+  //validate nested JSON Objects
+  expect(postAPIResponseBody.booking.bookingdates).toHaveProperty(
+    "checkin",
+    "2018-01-01"
+  );
+  expect(postAPIResponseBody.booking.bookingdates).toHaveProperty(
+    "checkout",
+    "2019-01-01"
+  );
+
+  //GET API call
+
+  console.log("==GET API==");
+
+  const getAPIResponse = await request.get(`/booking/${bId}`);
+  console.log(await getAPIResponse.json());
+
+  //validate status code
+
+  expect(getAPIResponse.ok()).toBeTruthy();
+  expect(getAPIResponse.status()).toBe(200);
+
+  //Generate Token
+
+  const tokenResponse = await request.post(`/auth`, {
+    data: tokenRequestBody,
+  });
+  const tokenAPIResponseBody = await tokenResponse.json();
+  const tokenNo = tokenAPIResponseBody.token;
+  console.log("Token no. is :" + tokenNo);
+
+  console.log("==PATCH API==");
+
+  //PUT API Call
+  const patchAPIResponse = await request.patch(`/booking/${bId}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: `token=${tokenNo}`,
+    },
+    data: patchRequestBody,
+  });
+  const patchAPIResponseBody = await patchAPIResponse.json();
+  console.log(patchAPIResponseBody);
+
+  //Validate status code
+
+  expect(patchAPIResponse.status()).toBe(200);
+});
